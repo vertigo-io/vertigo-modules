2979c0fb66609594ac04077ab7d56cc2b746054c
Matthieu Laroche <matthieu.laroche@telecom-bretagne.eu>
2023-12-21 14:58:59 +0100

    move modules to dedicated repos

--- vertigo-database/src/test/java/io/vertigo/database/migration/data/init.sql
+++
@@ -1,4 +0,0 @@
-create table movie ( 
-	id bigint,
-	title varchar(255)
-	);
--- vertigo-ui-vuejs/src/directives/VAutofocus.js
+++
@@ -1,9 +0,0 @@
-export default {
-    mounted: function(el, binding, vnode) {
-        var doFocus = binding.value;
-        if(doFocus && !window.autofocus) {
-             window.autofocus = true;
-             el.focus();
-        }
-     }
-}
